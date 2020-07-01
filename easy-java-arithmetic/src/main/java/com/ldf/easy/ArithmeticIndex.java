dbe349bd40f226e6face7486367eafd42c9a0b0f
lidefu <lidefu@xiaomi.com>
2020-07-01 19:26:07 +0800

    最长重复子数组

--- easy-java-arithmetic/src/main/java/com/ldf/easy/ArithmeticIndex.java
+++ easy-java-arithmetic/src/main/java/com/ldf/easy/ArithmeticIndex.java
@@ -1,5 +1,8 @@
 package com.ldf.easy;
 
+import com.ldf.easy.leetcode.CQueue;
+import com.ldf.easy.leetcode.KthLargestElementInArray;
+import com.ldf.easy.leetcode.LongestRepeatingSubArray;
 import com.ldf.easy.leetcode.MaximumSubsequenceSum;
 
 /**
@@ -13,6 +16,9 @@ public enum ArithmeticIndex {
      */
 
     最大子序和(MaximumSubsequenceSum.class),
+    数组中的第K个最大元素(KthLargestElementInArray.class),
+    用两个栈实现队列(CQueue.class),
+    最长重复子数组(LongestRepeatingSubArray.class),
 
     ;
 
