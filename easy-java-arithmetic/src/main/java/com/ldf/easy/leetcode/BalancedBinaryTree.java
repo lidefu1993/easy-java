fc60d03b6f93ae6a015d22fe69c08d1d76eb8fcf
lidefu <lidefu@xiaomi.com>
2020-08-20 10:55:18 +0800

    平衡二叉树-官方题解

--- easy-java-arithmetic/src/main/java/com/ldf/easy/leetcode/BalancedBinaryTree.java
+++ easy-java-arithmetic/src/main/java/com/ldf/easy/leetcode/BalancedBinaryTree.java
@@ -31,6 +31,23 @@ public class BalancedBinaryTree {
         System.out.println(tree.isBalanced(treeNode));
     }
 
+    /**
+     * 官方题解
+     */
+    public boolean isBalanced2(TreeNode root) {
+        return recur(root) != -1;
+    }
+
+    private int recur(TreeNode root) {
+        if (root == null) return 0;
+        int left = recur(root.left);
+        if(left == -1) return -1;
+        int right = recur(root.right);
+        if(right == -1) return -1;
+        return Math.abs(left - right) < 2 ? Math.max(left, right) + 1 : -1;
+    }
+
+
 
     class TreeNode{
         int val;
